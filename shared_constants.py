153ac9fa338e26eb124f5461bbad97bcc838833d
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2021-01-22 16:35:38 -0800

    test change

--- shared_constants.py
+++ shared_constants.py
@@ -5,10 +5,8 @@ import numpy as np
 
 ## Local Imports
 
-
 PI = np.pi
 TWOPI = 2*np.pi
 SPEED_OF_LIGHT = 3e8 # Speed of light in meters per second
 EPSILON = 1e-8 # Typically used to avoid division by 0
-
 
