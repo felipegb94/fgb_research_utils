73bcd1aff9986ad7977e8e69196c03805105c5ce
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2022-07-22 15:33:45 -0700

    update timer implementation

--- timer.py
+++ timer.py
@@ -5,12 +5,32 @@ import time
 
 ## Local Imports
 
-class Timer(object):
+class Timer:
 	def __init__(self, name=None):
 		self.name = name
-	def __enter__(self):
-		self.tstart = time.time()
-	def __exit__(self, type, value, traceback):
-		if self.name: print('[{}] - Elapsed: {} seconds.'.format(self.name, time.time() - self.tstart))
-		else: print('Elapsed: {}'.format(time.time() - self.tstart))
+		self._start = None
+		self.elapsed = 0.0
+
+	def start(self):
+		if self._start is not None:
+			raise RuntimeError('Timer already started...')
+		self._start = time.perf_counter()
+
+	def stop(self):
+		if self._start is None:
+			raise RuntimeError('Timer not yet started...')
+		end = time.perf_counter()
+		self.elapsed += end - self._start
+		self._start = None
+
+	def __enter__(self):  # Setup
+		self.start()
+		# return self so that we can use it outside of the with block, e.g. "with Timer as t:"
+		return self 
+
+	def __exit__(self, *args):  # Teardown
+		self.stop()
+		if self.name: print('[{}] - Elapsed: {} seconds.'.format(self.name, self.elapsed))
+		else: print('Elapsed: {}'.format(self.elapsed))
+				
 
